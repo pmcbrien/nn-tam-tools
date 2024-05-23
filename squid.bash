0c2b261822d4833b8af5077cc7aba28a7740d8b0
pat mcb <pmcbrien@users.noreply.github.com>
2024-05-23 09:05:38 -0400

    Create squid.bash

---
+++ squid.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo su -
+yum update -y
+yum install -y squid
+systemctl enable squid
+sed -i 's/http_access deny all/http_access allow all/' /etc/squid/squid.conf
+echo 'acl allow_my_ip src (YOUR_IP)/32' | sudo tee -a /etc/squid/squid.conf
+systemctl start squid
