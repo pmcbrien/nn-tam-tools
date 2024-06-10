3c8f90012a47eca65f0a9dd6db8ddfc207444d70
pat mcb <pmcbrien@users.noreply.github.com>
2024-06-10 13:16:05 -0400

    Update parseheaders.py

--- atprox/parseheaders.py
+++ atprox/parseheaders.py
@@ -1,23 +1,21 @@
 #mitmdump -q -v --set block_global=false -s parseheaders.py 
-
 def response(flow):
-    print("")
+    print("\n\n")
     print("="*50)
-    #print("FOR: " + flow.request.url)
     print(flow.request.method + " " + flow.request.path + " " + flow.request.http_version)
 
-    print("-"*5 + "request headers:")
+    print("\n-----" + "Request headers: -----")
     
     for k, v in flow.request.headers.items():
         print("%-20s: %s" % (k.upper(), v))
     
-    print("Request Body: ")
+    print("----- Request Body: ----- ")
     print(flow.request.content)
 
-    print("-"*5 + "response headers:")
+    print("\n-----" + "Response headers: -----")
     for k, v in flow.response.headers.items():
         print("%-20s: %s" % (k.upper(), v))
-        print("-"*5 + "request headers:")
+        #print("-"*5 + "request headers:")
 
-    print("Response Body:")
+    print("----- Response Body:")
     print(flow.response.content)
