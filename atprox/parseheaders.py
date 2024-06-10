7ce6461f656545b361736baa7bac453494c1531b
pat mcb <pmcbrien@users.noreply.github.com>
2024-06-10 12:19:34 -0400

    Create parseheaders.py

---
+++ atprox/parseheaders.py
@@ -0,0 +1,21 @@
+def response(flow):
+    print("")
+    print("="*50)
+    #print("FOR: " + flow.request.url)
+    print(flow.request.method + " " + flow.request.path + " " + flow.request.http_version)
+
+    print("-"*5 + "request headers:")
+    
+    for k, v in flow.request.headers.items():
+        print("%-20s: %s" % (k.upper(), v))
+    
+    print("Request Body: ")
+    print(flow.request.content)
+
+    print("-"*5 + "response headers:")
+    for k, v in flow.response.headers.items():
+        print("%-20s: %s" % (k.upper(), v))
+        print("-"*5 + "request headers:")
+
+    print("Response Body:")
+    print(flow.response.content)
